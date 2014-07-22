f0872c94ca876acb7d308c800428cb88975d7cd5
rafatpc <rafa_tpc@live.co.uk>
2014-07-22 18:04:01 +0300

    Don't repeat yourself (DRY)

--- engine/init.js
+++ engine/init.js
@@ -4,53 +4,17 @@ $(function() {
     var moveSpeed = 15;
     var maxLeft = $("#wrapper").width() - $("#player").width();
     var maxTop = $("#wrapper").height() - $("#player").height();
-    
+
     addEventListener("keydown", function(e) {
         if (interval == null) {
             if (e.keyCode == 37) {
-                interval = setInterval(function() {
-                    var leftPos = parseInt($("#player").css("left"));
-                    var newPosition = leftPos - moveSpeed;
-
-                    if (newPosition < 0) {
-                        newPosition = 0;
-                    }
-
-                    $("#player").css({left: newPosition});
-                }, 50);
+                interval = move.movePlayer("left", "-", moveSpeed);
             } else if (e.keyCode == 38) {
-                interval = setInterval(function() {
-                    var topPos = parseInt($("#player").css("top"));
-                    var newPosition = topPos - moveSpeed;
-
-                    if (newPosition < 0) {
-                        newPosition = 0;
-                    }
-
-                    $("#player").css({top: newPosition});
-                }, 50);
+                interval = move.movePlayer("top", "-", moveSpeed);
             } else if (e.keyCode == 39) {
-                interval = setInterval(function() {
-                    var leftPos = parseInt($("#player").css("left"));
-                    var newPosition = leftPos + moveSpeed;
-
-                    if (newPosition > maxLeft) {
-                        newPosition = maxLeft;
-                    }
-
-                    $("#player").css({left: newPosition});
-                }, 50);
+                interval = move.movePlayer("left", "+", moveSpeed, maxLeft);
             } else if (e.keyCode == 40) {
-                interval = setInterval(function() {
-                    var topPos = parseInt($("#player").css("top"));
-                    var newPosition = topPos + moveSpeed;
-
-                    if (newPosition > maxTop) {
-                        newPosition = maxTop;
-                    }
-
-                    $("#player").css({top: newPosition});
-                }, 50);
+                interval = move.movePlayer("top", "+", moveSpeed, maxTop);
             }
         }
 
@@ -65,8 +29,4 @@ $(function() {
 
         move.unSetKeyDown(e.keyCode);
     }, false);
-
-    $("#test").click(function() {
-        move.geyKeysDown();
-    });
 });
--- engine/movement.js
+++ engine/movement.js
@@ -1,11 +1,11 @@
 var movement = function() {
     var keysDown = {};
-    
+
     this.setKeyDown = function(keyCode) {
         keysDown[keyCode] = true;
         return this.keysDown;
     };
-    
+
     this.unSetKeyDown = function(keyCode) {
         delete keysDown[keyCode];
         return this.keysDown;
@@ -13,5 +13,32 @@ var movement = function() {
 
     this.isKeyDown = function(keyCode) {
         return this.keysDown[keyCode];
+    };
+
+    this.movePlayer = function(direction, type, moveSpeed, limit) {
+        return setInterval(function() {
+            var currentPosition = parseInt($("#player").css(direction));
+            
+            if(type == "-"){
+                var newPosition = currentPosition - moveSpeed;
+            } else {
+                var newPosition = currentPosition + moveSpeed;
+            }
+            
+            
+            if (newPosition < 0 || newPosition > limit) {
+                if(limit == undefined){
+                    newPosition = 0;
+                } else {
+                    newPosition = limit;
+                }
+            }
+            
+            if(direction == "top"){
+                $("#player").css({top: newPosition});
+            } else {
+                $("#player").css({left: newPosition});
+            }
+        }, 50);
     };
 };
