31e913e9958f44ee1b0338b661fb022b1d675576
rafatpc <rafa_tpc@live.co.uk>
2014-07-22 18:34:20 +0300

    Movement encapsulation

--- engine/init.js
+++ engine/init.js
@@ -1,32 +1,8 @@
 $(function() {
     var move = new movement();
-    var interval = null;
-    var moveSpeed = 15;
-    var maxLeft = $("#wrapper").width() - $("#player").width();
-    var maxTop = $("#wrapper").height() - $("#player").height();
+    var collision = new objectCollision();
 
-    addEventListener("keydown", function(e) {
-        if (interval == null) {
-            if (e.keyCode == 37) {
-                interval = move.movePlayer("left", "-", moveSpeed);
-            } else if (e.keyCode == 38) {
-                interval = move.movePlayer("top", "-", moveSpeed);
-            } else if (e.keyCode == 39) {
-                interval = move.movePlayer("left", "+", moveSpeed, maxLeft);
-            } else if (e.keyCode == 40) {
-                interval = move.movePlayer("top", "+", moveSpeed, maxTop);
-            }
-        }
-
-        move.setKeyDown(e.keyCode);
-    }, false);
-
-    addEventListener("keyup", function(e) {
-        if (interval != null) {
-            clearInterval(interval);
-            interval = null;
-        }
-
-        move.unSetKeyDown(e.keyCode);
-    }, false);
+    move.__construct();
+    
+    collision.getQuestsCoordinates();
 });
--- engine/movement.js
+++ engine/movement.js
@@ -1,44 +1,79 @@
 var movement = function() {
     var keysDown = {};
+    var interval = null;
+    var moveSpeed = 15;
+    var maxLeft = $("#wrapper").width() - $("#player").width();
+    var maxTop = $("#wrapper").height() - $("#player").height();
 
-    this.setKeyDown = function(keyCode) {
+    function setKeyDown(keyCode) {
         keysDown[keyCode] = true;
         return this.keysDown;
     };
 
-    this.unSetKeyDown = function(keyCode) {
+    function unSetKeyDown(keyCode) {
         delete keysDown[keyCode];
         return this.keysDown;
     };
 
-    this.isKeyDown = function(keyCode) {
-        return this.keysDown[keyCode];
-    };
-
-    this.movePlayer = function(direction, type, moveSpeed, limit) {
+    function movePlayer(direction, type, moveSpeed, limit) {
         return setInterval(function() {
             var currentPosition = parseInt($("#player").css(direction));
-            
-            if(type == "-"){
+
+            if (type == "-") {
                 var newPosition = currentPosition - moveSpeed;
             } else {
                 var newPosition = currentPosition + moveSpeed;
             }
-            
-            
+
             if (newPosition < 0 || newPosition > limit) {
-                if(limit == undefined){
+                if (limit == undefined) {
                     newPosition = 0;
                 } else {
                     newPosition = limit;
                 }
             }
-            
-            if(direction == "top"){
+
+            if (direction == "top") {
                 $("#player").css({top: newPosition});
             } else {
                 $("#player").css({left: newPosition});
             }
         }, 50);
+    };
+    
+    function keyDownListener() {
+        addEventListener("keydown", function(e) {
+            if (interval == null) {
+                if (e.keyCode == 37) {
+                    interval = movePlayer("left", "-", moveSpeed);
+                } else if (e.keyCode == 38) {
+                    interval = movePlayer("top", "-", moveSpeed);
+                } else if (e.keyCode == 39) {
+                    interval = movePlayer("left", "+", moveSpeed, maxLeft);
+                } else if (e.keyCode == 40) {
+                    interval = movePlayer("top", "+", moveSpeed, maxTop);
+                }
+            }
+
+            setKeyDown(e.keyCode);
+        }, false);
+    };
+    
+    function keyUpListener() {
+        addEventListener("keyup", function(e) {
+            if (interval != null) {
+                clearInterval(interval);
+                interval = null;
+            }
+
+            unSetKeyDown(e.keyCode);
+        }, false);
+
+        return interval;
+    };
+    
+    this.__construct = function (){
+        keyDownListener();
+        keyUpListener();
     };
 };
