84f41718ed0b49b2cf924096b619ebb199fc5951
rafatpc <rafa_tpc@live.co.uk>
2014-07-22 21:55:51 +0300

    Cross-browser JavaScript

--- engine/questCollision.js
+++ engine/questCollision.js
@@ -1,15 +1,16 @@
 var questCollision = function() {
     var questCoordinates = {};
-    var playerCoords = {left: 0, top: 0};
+    var playerCoords = {left: 0, top: 0, right: 0};
     var playerHeight = $("#player").height();
     var playerWidth = $("#player").width();
+    var wrapperWidth = $("#wrapper").width();
     var overQuest = null;
     var radius = 50;
-    
+
     function getQuestsCoordinates() {
         $(".quest").each(function() {
             var id = $(this).attr("id");
-            var leftPos = parseInt($(this).css("left"));
+            var leftPos = parseInt($(this).css("left")) || (wrapperWidth - parseInt($(this).css("right"))- $(this).width());
             var topPos = parseInt($(this).css("top"));
             questCoordinates[id] = {left: leftPos, top: topPos};
         });
@@ -17,9 +18,9 @@ var questCollision = function() {
 
     function getPlayerCoordinates() {
         setInterval(function() {
-            var posLeft = parseInt($("#player").css("left"));
-            var posRight = parseInt($("#player").css("top"));
-            var posTop = parseInt($("#player").css("top"));
+            var posLeft = parseInt($("#player").css("left") || 0);
+            var posRight = wrapperWidth - playerWidth - posLeft;
+            var posTop = parseInt($("#player").css("top") || 0);
             playerCoords = {left: posLeft, top: posTop, right: posRight};
         }, 1000);
     }
@@ -28,30 +29,28 @@ var questCollision = function() {
         setInterval(function() {
             for (var key in questCoordinates) {
                 var obj = questCoordinates[key];
+                var questWidth = $("#" + key).width();
+                var right = wrapperWidth - questWidth - obj.left;
                 var topStart = obj.top - playerHeight - radius;
                 var topEnd = obj.top + playerHeight + radius;
                 var leftStart = obj.left - playerWidth - radius;
-                var leftEnd = obj.left + playerWidth + $("#" + key).width() + radius;
-                var rightStart = parseInt($("#lecturer-2").css("right")) + radius + playerWidth;
-                var rightEnd = parseInt($("#lecturer-2").css("right")) - radius - playerWidth;
+                var leftEnd = obj.left + playerWidth + questWidth + radius;
+                var rightStart = right + radius + playerWidth;
+                var rightEnd = right - radius - playerWidth;
                 var playerTop = playerCoords.top;
                 var playerLeft = playerCoords.left;
                 var playerRight = playerCoords.right;
-
-                if (rightEnd < 0) {
-                    rightEnd = 0;
-                }
-
+                
                 var topExpression = (playerTop >= topStart && playerTop <= topEnd);
 
                 if ((topExpression && playerLeft >= leftStart && playerLeft <= leftEnd) ||
-                        (topExpression && rightStart >= playerRight && rightEnd >= playerRight)) {
+                        (topExpression && rightStart >= playerRight && rightEnd >= playerRight && obj.left > (wrapperWidth / 2))) {
                     overQuest = key;
                 } else if (key == overQuest) {
                     overQuest = null;
                 }
             }
-        }, 1000);
+        }, 200);
     }
 
     this.__construct = function() {
@@ -59,8 +58,8 @@ var questCollision = function() {
         getPlayerCoordinates();
         questCollisionChecker();
     };
-    
-    this.getOverQuest = function (){
+
+    this.getOverQuest = function() {
         return overQuest;
     };
 };
