a22d762157e3a62306d153b71218be2c897abbef
rafatpc <rafa_tpc@live.co.uk>
2014-07-30 21:30:55 +0300

    Character movement animation

--- engine/movement.js
+++ engine/movement.js
@@ -1,6 +1,8 @@
 var movement = function() {
     var interval = null;
-    var moveSpeed = 10;
+    var animateInterval = null;
+    var moveSpeed = 5;
+    var step = 0;
     var maxLeft = $("#wrapper").width() - $("#player").width();
     var maxTop = $("#wrapper").height() - $("#player").height();
     var lastKeyPressed = 0;
@@ -31,19 +33,44 @@ var movement = function() {
         }, 50);
     }
 
+    function animate(type) {
+        var yPosition = type * 32 + "px";
+
+        animateMovement(yPosition);
+        animateInterval = setInterval(function() {
+            animateMovement(yPosition);
+        }, 100);
+    }
+
+    function animateMovement(yPosition) {
+        var xPosition = step * 30 + "px";
+
+        if (step == 3) {
+            step = 0;
+        }
+
+        $('#player').css('background-position', xPosition + ' ' + yPosition);
+
+        step++;
+    }
+
     function keyDownListener() {
         addEventListener("keydown", function(e) {
             var key = e.keyCode;
 
-            if (interval == null && !$("#dialog").is(":visible") ) {
+            if (interval == null && !$("#dialog").is(":visible")) {
                 if (key == 37) {  //left arrow
                     interval = movePlayer("left", "-", moveSpeed);
+                    animate(3);
                 } else if (key == 38) { //up arrow
                     interval = movePlayer("top", "-", moveSpeed);
+                    animate(1);
                 } else if (key == 39) { //right arrow
+                    animate(2);
                     interval = movePlayer("left", "+", moveSpeed, maxLeft);
                 } else if (key == 40) { //down arrow
                     interval = movePlayer("top", "+", moveSpeed, maxTop);
+                    animate(0);
                 }
 
                 lastKeyPressed = key;
@@ -56,7 +83,9 @@ var movement = function() {
             var key = e.keyCode;
 
             if (interval != null && key >= 37 && key <= 40) {
+                clearInterval(animateInterval);
                 clearInterval(interval);
+                animateInterval = null;
                 interval = null;
             }
         }, false);
